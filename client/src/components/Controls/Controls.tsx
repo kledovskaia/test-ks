ffc884ab861e051ee1c09fa019d16fc504efe4f0
Kseniia <kseniialedovskaia@gmail.com>
2022-06-13 18:55:36 +0300

    select server communication

--- client/src/components/Controls/Controls.tsx
+++ client/src/components/Controls/Controls.tsx
@@ -1,7 +1,12 @@
-import { DetailedHTMLProps, FC, HTMLAttributes, memo } from 'react'
+import { ChangeEvent, DetailedHTMLProps, FC, HTMLAttributes, memo } from 'react'
 import cn from 'classnames'
 import styles from './Controls.module.scss'
 import { DataContext } from '../../context/dataContext'
+import { useCallback } from 'react'
+import Select from '../Select/Select'
+
+const fieldOptions = ['Название', 'Количество', 'Расстояние']
+const orderOptions = ['Равно', 'Содержит', 'Больше', 'Меньше']
 
 type Props = {
   state: DataContext['state']
@@ -9,9 +14,30 @@ type Props = {
 } & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 
 const Controls: FC<Props> = ({ state, updateState, className, ...props }) => {
+  const handleUpdate = useCallback((event: ChangeEvent<HTMLSelectElement>) => {
+    updateState(
+      event.target.name as keyof RequestParams,
+      event.target.value,
+    )
+  }, [updateState])
+
+
   return (
     <div className={cn(className, styles.controls)} {...props}>
-      controls
+      <Select 
+        label="Колонка:"
+        options={fieldOptions}
+        value={state.field || ''}
+        onChange={handleUpdate}
+        name="field"
+      />
+      <Select 
+        label="Условие:"
+        options={orderOptions}
+        value={state.order || ''}
+        onChange={handleUpdate}
+        name="order"
+      />
     </div>
   )
 }
--- client/src/components/Select/Select.tsx
+++ client/src/components/Select/Select.tsx
@@ -1,14 +1,30 @@
-import { DetailedHTMLProps, FC, HTMLAttributes, memo } from 'react'
+import { ChangeEvent, DetailedHTMLProps, FC, HTMLAttributes, memo, useCallback, useState } from 'react'
 import cn from 'classnames'
 import styles from './Select.module.scss'
 
-type Props = DetailedHTMLProps<
+type Props = {
+  label: string,
+  options: string[],
+  name: string,
+  value: string,
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void
+} & DetailedHTMLProps<
   HTMLAttributes<HTMLSelectElement>,
   HTMLSelectElement
 >
 
-const Select: FC<Props> = ({ className, ...props }) => {
-  return <select className={cn(className, styles.select)} {...props}></select>
+const Select: FC<Props> = ({ label, options, className, ...props }) => {
+  return (
+    <select 
+      className={cn(className, styles.select)} 
+      {...props}
+    >
+      <option value="" disabled>{label}</option>
+      {options.map(option => (
+        <option key={option} value={option.toLowerCase()}>{option}</option>
+      ))}
+    </select>
+  )
 }
 
 export default memo(Select)
